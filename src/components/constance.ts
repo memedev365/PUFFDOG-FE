56ef9ef97eb65769d4c067717bdf04d6797c0b4f
memedev365 <memedev365@outlook.com>
2025-04-23 19:00:13 +0530

    Update constance.ts

--- src/components/constance.ts
+++ src/components/constance.ts
@@ -1,7 +1,7 @@
 // src/config/constants.ts
 
 // Admin wallet address for airdrop authorization
-export const ADMIN_WALLET_ADDRESS = '7eLQnG9QoqdqtPbpXxQ3kSJXTTjWdGXK8SeVeHR7HnsM'; // Replace with your actual admin wallet address
+export const ADMIN_WALLET_ADDRESS = '4mGCSmGmfAfq7uvLpV39uQRTLuveGX2EHk6iuN38YRLn'; // Replace with your actual admin wallet address
 
 // NFT collection constants
 export const MAX_SUPPLY = 10000;
@@ -9,5 +9,5 @@ export const LAMPORTS_PER_SOL = 1000000000;
 export const perNFTPrice = 0; // Price in SOL per NFT
 
 // Contract addresses
-export const tokenAddress = '8Xg64LeT1HYZNbv9PMLf8hbUvULwm9pNDfKk1oofePtu';
-export const merkleTreeAddress = 'CMLK4GDt1v74tDDtt9zzpwfvb17ESMQqMUMp3fKsjDAF';
+export const tokenAddress = '73itZp41Td5nj8z2AnQhGmbequoqtPNXvjxbDw1hj3Rn';
+export const merkleTreeAddress = '8ZhNxjgfDmSuaJvUoKek12kSpC2nebucgqZzVMAnhxCq';
