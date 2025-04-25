a56fa13794bba4790b5b8168a268b1ab210c0e73
memedev365 <memedev365@outlook.com>
2025-04-25 10:36:11 +0530

    Update treebubble.tsx

--- src/components/treebubble.tsx
+++ src/components/treebubble.tsx
@@ -399,7 +399,7 @@ async function setAndVerifyCollection() {
                 
             const response = await axios.post(
                 apiUrl,
-                {leafIndex: 4},
+                {leafIndex: 1},
                 {
                     headers: {
                         'Content-Type': 'application/json',
